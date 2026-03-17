8547e7b6403273e85c973f592ca14a242dcd4ada
Hyunho Kook <kook@srv1498477.hstgr.cloud>
2026-03-17 04:09:37 +0000

    Add dynamic status loading from GitHub

---
+++ stores-data.js
@@ -0,0 +1,35 @@
+        const stores = [
+            {
+                id: 'nintendo',
+                name: '닌텐도 공식 스토어',
+                logo: 'N',
+                color: 'nintendo',
+                status: 'soldout',
+                products: [
+                    { name: 'Nintendo Switch 2', price: '₩648,000', url: 'https://store.nintendo.co.kr/physical/switch2-products', image: '🎮' },
+                    { name: 'Switch 2 마리오카트 번들', price: '₩688,000', url: 'https://store.nintendo.co.kr/physical/switch2-products', image: '🏎️' }
+                ]
+            },
+            {
+                id: 'himart',
+                name: '하이마트',
+                logo: 'H',
+                color: 'himart',
+                status: 'soldout',
+                products: [
+                    { name: '닌텐도 스위치2 본체', price: '₩648,000', url: 'https://m.e-himart.co.kr/app/goods/goodsDetail?goodsNo=0049455365', image: '🎮' },
+                    { name: '닌텐도 스위치2 마리오카트 번들', price: '₩688,000', url: 'https://m.e-himart.co.kr/app/goods/goodsDetail?goodsNo=0049455367', image: '🏎️' }
+                ]
+            },
+            {
+                id: 'eleven',
+                name: '11번가',
+                logo: '11',
+                color: 'eleven',
+                status: 'soldout',
+                products: [
+                    { name: '닌텐도 스위치2 본체', price: '₩648,000', url: 'https://www.11st.co.kr/products/8369248443', image: '🎮' },
+                    { name: '닌텐도 스위치2 마리오카트 번들', price: '₩688,000', url: 'https://www.11st.co.kr/products/8369253585', image: '🏎️' }
+                ]
+            }
+        ];
